53bdd90e2e74580e944bc3cc8c1865c6b5d00a9f
Chanpoe <941407617@qq.com>
2025-02-08 22:14:44 +0800

    feat HelloWorld

--- frontend/pages/index/index.js
+++ frontend/pages/index/index.js
@@ -11,6 +11,7 @@ Page({
     hasUserInfo: false,
     canIUseGetUserProfile: wx.canIUse('getUserProfile'),
     canIUseNicknameComp: wx.canIUse('input.type.nickname'),
+    responseData: "" // 存储从后端获取的数据
   },
   bindViewTap() {
     wx.navigateTo({
@@ -45,5 +46,25 @@ Page({
         })
       }
     })
+  },
+    // 点击按钮时触发的函数
+  getData: function() {
+    wx.request({
+      url: 'http://127.0.0.1:5050/',  // 后端接口地址
+      method: 'GET',  // 请求方式
+      success: (res) => {
+        // 请求成功时的回调
+        this.setData({
+          responseData: res.data  // 将返回的数据存储到data中，更新页面
+        });
+      },
+      fail: (err) => {
+        // 请求失败时的回调
+        console.error("请求失败", err);
+        this.setData({
+          responseData: "请求失败"  // 请求失败时的提示
+        });
+      }
+    });
   },
 })
