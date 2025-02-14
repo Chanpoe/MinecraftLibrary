71222be97178e564966db15cb8829c0599968f13
S-a-i_ <1017667082@qq.com>
2025-02-15 00:40:52 +0800

    单例模式

--- backend/Controller/DatabaseController.py
+++ backend/Controller/DatabaseController.py
@@ -2,6 +2,11 @@ import config
 import pymongo
 
 class DatabaseController():
+    _instance=None
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(DatabaseController, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
     def __init__(self):
         self.db=pymongo.MongoClient(f"mongodb://{config.USERNAME}:{config.PASSWORD}@{config.IP}")["MinecraftLibrary"]
 
@@ -12,3 +17,8 @@ class DatabaseController():
             if item.get("name",'')==item_name:
                 return str(item)
         return "collection request failed!!"
+
+if __name__=="__main__":
+    a=DatabaseController()
+    b=DatabaseController()
+    print(a is b)
